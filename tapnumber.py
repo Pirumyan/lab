1b2e6e90f74a9d17a167a7dbbd3580b3494077ae
Pirumyan <gorpirum@gmail.com>
2023-12-07 23:31:48 +0400

    vrode jisht em haskacel

---
+++ tapnumber.py
@@ -0,0 +1,19 @@
+def tap(matrix: list) -> list:
+    res = []
+    syunner, toxer = len(matrix), len(matrix[0])
+
+    for i in range(syunner):
+        for j in range(toxer):
+            element = matrix[i][j]
+            if element == min(matrix[i]) and element == max(matrix[k][j] for k in range(syunner)):
+                res.append((i, j, element))
+
+    return res if len(res) > 0 else None
+
+
+# Example usage:
+matrix = [[3, 1, 4],
+          [5, 2, 6],
+          [9, 7, 8]]
+
+print(tap(matrix))
